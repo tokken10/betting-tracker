2c4eaf0a9543f0e64dce27d13d7976e1b37beb36
tokken10 <65119960+tokken10@users.noreply.github.com>
2025-08-07 23:30:59 -0700

    Add date formatting utility

---
+++ js/utils.js
@@ -0,0 +1,7 @@
+export function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const year = date.getUTCFullYear();
+  return `${month}-${day}-${year}`;
+}
