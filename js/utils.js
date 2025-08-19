25634903510bcb0ec5237e4e6620d992558bae43
tokken10 <65119960+tokken10@users.noreply.github.com>
2025-08-18 20:00:12 -0700

    Escape user-provided text in bet views

--- js/utils.js
+++ js/utils.js
@@ -13,3 +13,12 @@ export function decodeToken(token) {
     return null;
   }
 }
+
+export function escapeHtml(str) {
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
