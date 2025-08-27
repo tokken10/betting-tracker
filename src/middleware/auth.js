a170509bbd46cf8743e43ef54cced5789a11b1af
tokken10 <65119960+tokken10@users.noreply.github.com>
2025-08-26 17:50:29 -0700

    feat: support cookie auth tokens

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,10 @@ import jwt from 'jsonwebtoken';
 
 export default function (req, res, next) {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  let token = req.cookies?.token;
+  if (!token && authHeader) {
+    token = authHeader.split(' ')[1];
+  }
   if (!token) {
     return res.status(401).json({ error: 'No token provided' });
   }
